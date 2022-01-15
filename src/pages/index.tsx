340f7cf804ce07d8c93ecb7d0a4b198878cd00f5
qwwerty <rhalfsouzaa@gmail.com>
2022-01-15 01:37:19 -0300

    feature: adicionar pagina de em construção.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1 className="font-black">Hello.js</h1>
+    <main className="flex flex-col items-center justify-center w-full h-screen min-w-full min-h-screen p-5 bg-red-500 gap-10">
+      <section className="flex items-center justify-center w-full sm:w-1/2">
+        <img src="images/construction.png" alt="logo de construção"/>
+      </section>
+
+      <section className="text-center">
+        <h1 className="text-2xl font-black text-white sm:text-3xl">Desculpe, o site está em construção</h1>
+      </section>
+    </main>
   )
 }
