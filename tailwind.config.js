d64e953b8e248f96393b27e79127e009a9905285
qwwerty <rhalfsouzaa@gmail.com>
2022-01-22 16:58:53 -0300

    feature: add tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-main': '#010F1D'
+      }
+    },
+    fontFamily: {
+      'grotesk': ['Space Grotesk', 'sans-serif'],
+      'overpass': ['Overpass', 'sans-serif']
+    }
   },
   variants: {
     extend: {},
