3a36c308c587e4855368aca19504695eb49aad42
qwwerty <rhalfsouzaa@gmail.com>
2022-01-23 19:34:26 -0300

    feature: icon component hamburguer

---
+++ src/components/Icons/Hamburguer/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+
+interface HamburProps {
+  isOpen: boolean;
+}
+
+export function Hamburguer({ isOpen = false }: HamburProps) {
+  return (
+    <div
+      className={`${styles.hamburguer} absolute top-0 right-0 ${
+        isOpen && styles["close-hamburguer"]
+      }`}
+    >
+      <div className={`${styles.lines} ${isOpen && styles["line-top"]}`}></div>
+      <div className={`${styles.lines} ${isOpen && styles["line-mid"]}`}></div>
+      <div
+        className={`${styles.lines} ${isOpen && styles["line-bottom"]}`}
+      ></div>
+    </div>
+  );
+}
