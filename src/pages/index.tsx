58f4d2407711f4a54b0744f7c63a973c5fc8bd7f
qwwerty <rhalfsouzaa@gmail.com>
2022-01-23 12:32:04 -0300

    feature: create header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
+import { Header } from './components/Login/Header'
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-full h-screen min-w-full min-h-screen p-5 bg-red-500 gap-10">
-      <section className="flex items-center justify-center w-full sm:w-1/2">
-        <img src="images/construction.png" alt="logo de construção"/>
-      </section>
-
-      <section className="text-center">
-        <h1 className="text-2xl font-black text-white sm:text-3xl">Desculpe, o site está em construção</h1>
-      </section>
+    <main className="w-full max-w-full min-h-screen bg-bg-main">
+      <Header />
     </main>
   )
 }
