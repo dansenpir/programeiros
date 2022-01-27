60abd20e1d4f7bae3603178906e79cad33e1cc6b
dansenpir <senegalha@protonmail.com>
2022-01-27 10:18:44 -0300

    feat: add social links

--- src/pages/components/Home/Social/index.tsx
+++ src/pages/components/Home/Social/index.tsx
@@ -3,21 +3,27 @@ import styles from './styles.module.css';
 export default function Social() {
   return (
     <div className={styles.social}>
-      <img
+      <a
         className={styles.icon}
-        src='/images/GithubLogo.svg'
-        alt='Logo GitHub'
-      />
-      <img
+        href='https://github.com/Programeiros'
+        target='_blank'
+        rel='noreferrer'>
+        <img src='/images/GithubLogo.svg' alt='Logo GitHub' />
+      </a>
+      <a
         className={styles.icon}
-        src='/images/TelegramLogo.svg'
-        alt='Logo GitHub'
-      />
-      <img
+        href='https://t.me/+p8PHV0M348k3NjE5'
+        target='_blank'
+        rel='noreferrer'>
+        <img src='/images/TelegramLogo.svg' alt='Logo GitHub' />
+      </a>
+      <a
         className={styles.icon}
-        src='/images/DiscordLogo.svg'
-        alt='Logo GitHub'
-      />
+        href='https://discord.gg/GGjVCppR9a'
+        target='_blank'
+        rel='noreferrer'>
+        <img src='/images/DiscordLogo.svg' alt='Logo GitHub' />
+      </a>
     </div>
   );
 }
