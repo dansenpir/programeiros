ce35749f05633bde7aead0eb469908698248c18e
dansenpir <senegalha@protonmail.com>
2022-01-27 09:51:32 -0300

    fix: add spacing

--- src/pages/components/Home/Footer/index.tsx
+++ src/pages/components/Home/Footer/index.tsx
@@ -3,20 +3,22 @@ import styles from './styles.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={`${styles.footerSocial} ${styles.bgBlack}`}>
-        <img src='/images/p_logo_blue_white.svg' alt='Logo Programeiros' />
-        <Social />
+    <footer className={`${styles.footer}`}>
+      <div className={styles.bgBlack}>
+        <div className={`px-24 ${styles.footerSocial}`}>
+          <img src='/images/p_logo_blue_white.svg' alt='Logo Programeiros' />
+          <Social />
+        </div>
       </div>
       <div className={styles.bgBlue}>
-        <div className={styles.footerCopyright}>
+        <div className={`px-24 ${styles.footerCopyright}`}>
           <div className={styles.footerInfo}>
             <p>Termos</p>
             <p className={styles.footerInfoItem}>Privacidade</p>
             <p>Contato</p>
           </div>
           <div>
-            <p>©{new Date().getFullYear()} Programeiros</p>
+            <p>© {new Date().getFullYear()} Programeiros</p>
           </div>
         </div>
       </div>
--- src/pages/components/Home/Social/index.tsx
+++ src/pages/components/Home/Social/index.tsx
@@ -3,13 +3,21 @@ import styles from './styles.module.css';
 export default function Social() {
   return (
     <div className={styles.social}>
-      <img src='/images/GithubLogo.svg' alt='Logo GitHub' />
+      <img
+        className={styles.icon}
+        src='/images/GithubLogo.svg'
+        alt='Logo GitHub'
+      />
       <img
         className={styles.icon}
         src='/images/TelegramLogo.svg'
         alt='Logo GitHub'
       />
-      <img src='/images/DiscordLogo.svg' alt='Logo GitHub' />
+      <img
+        className={styles.icon}
+        src='/images/DiscordLogo.svg'
+        alt='Logo GitHub'
+      />
     </div>
   );
 }
