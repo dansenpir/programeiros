687554c7e3bf75a23b54f050b3d5cef0576dc333
dansenpir <senegalha@protonmail.com>
2022-01-27 20:24:30 -0300

    fix: improve highlight component responsiveness

--- src/components/TelegramButton/index.tsx
+++ src/components/TelegramButton/index.tsx
@@ -1,11 +1,13 @@
-import styles from './styles.module.css';
-
 export default function TelegramButton() {
   return (
     <a href='https://t.me/+p8PHV0M348k3NjE5' target='_blank' rel='noreferrer'>
-      <button className={styles.btn}>
-        <img src='/images/telegram_logo.svg' alt='Logo do Telegram' />
-        <p className={styles.text}>Conheça nosso Telegram</p>
+      <button className='flex items-center border-1 border-solid border-btn-border rounded-xl py-1 px-4 md:px-5 lg:py-2 lg:px-5 xl:py-2.5 xl:px-10 text-xs lg:text-base text-btn-border font-grotesk font-normal leading-5'>
+        <img
+          className='w-4 lg:w-6 xl:w-8'
+          src='/images/telegram_logo.svg'
+          alt='Logo do Telegram'
+        />
+        <p className='ml-4'>Conheça nosso Telegram</p>
       </button>
     </a>
   );
