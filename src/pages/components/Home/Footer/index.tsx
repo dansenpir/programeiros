4446bb7333eab15f4b1f8fe8986ec8dfde130e7a
dansenpir <senegalha@protonmail.com>
2022-01-27 21:31:06 -0300

    fix: improve footer component responsiveness

--- src/pages/components/Home/Footer/index.tsx
+++ src/pages/components/Home/Footer/index.tsx
@@ -1,23 +1,24 @@
-import Social from '../Social';
-import styles from './styles.module.css';
+import Social from './Social';
 
 export default function Footer() {
   return (
-    <footer className={`${styles.footer}`}>
-      <div className={styles.bgBlack}>
-        <div className={`px-24 ${styles.footerSocial}`}>
-          <img src='/images/p_logo_blue_white.svg' alt='Logo Programeiros' />
+    <footer>
+      <div className='bg-bg-main py-12 md:py-20'>
+        <div className='flex items-center justify-between px-10 xl:px-24'>
+          <div className='w-1/4 md:w-2/12 xl:w-full'>
+            <img src='/images/p_logo_blue_white.svg' alt='Logo Programeiros' />
+          </div>
           <Social />
         </div>
       </div>
-      <div className={styles.bgBlue}>
-        <div className={`px-24 ${styles.footerCopyright}`}>
-          <div className={styles.footerInfo}>
+      <div className='bg-p-shadow py-6 md:py-20'>
+        <div className='flex justify-between text-white font-overpass font-normal text-xs lg:text-base leading-6 px-10 xl:px-24'>
+          <div className='flex flex-col md:flex-row'>
             <p>Termos</p>
-            <p className={styles.footerInfoItem}>Privacidade</p>
+            <p className='md:px-16'>Privacidade</p>
             <p>Contato</p>
           </div>
-          <div>
+          <div className='flex items-center md:flex-none'>
             <p>© {new Date().getFullYear()} Programeiros</p>
           </div>
         </div>
