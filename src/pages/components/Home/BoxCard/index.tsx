dba9a06a7dce3cb50828f29b30919f4cd7ec28bf
dansenpir <senegalha@protonmail.com>
2022-03-30 16:08:38 -0300

    refactor: update BoxCard texts

--- src/pages/components/Home/BoxCard/index.tsx
+++ src/pages/components/Home/BoxCard/index.tsx
@@ -4,9 +4,17 @@ import Card from './Card';
 export default function BoxCard() {
   return (
     <section>
-      <h1 className={`${styles.textRainbow} font-grotesk font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl px-10 xl:px-24 xl:leading-24 mt-36 w-4/5 md:w-3/5`}>
-        Mergulhe no mundo da tecnologia
-      </h1>
+      <div className='px-10 xl:px-24'>
+        <h1
+          className={`${styles.textRainbow} font-grotesk font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl xl:leading-24 mt-36 w-4/5 md:w-3/5`}>
+          Está pronto para começar?
+        </h1>
+        <p className='font-overpass font-light text-xl mt-7 w-7/12'>
+          Nós do Programeiros, projetamos esta plataforma para te auxiliar no
+          seu desenvolvimento profissional e para isso trouxemos soluções que
+          irão te ajudar no percurso.
+        </p>
+      </div>
       <div className='flex flex-col md:flex-row justify-between mt-16 mx-10 md:mx-0'>
         <Card
           titleColor={'text-orange'}
