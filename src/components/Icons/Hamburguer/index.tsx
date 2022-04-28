66f72a8d464212848155c01487554a08976c907f
dansenpir <dansenpir@outlook.com>
2022-04-28 16:40:47 -0300

    fix: set dark mode classes

--- src/components/Icons/Hamburguer/index.tsx
+++ src/components/Icons/Hamburguer/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
 
 interface HamburProps {
   isOpen: boolean;
@@ -6,28 +6,23 @@ interface HamburProps {
 
 export function Hamburguer({ isOpen = false }: HamburProps) {
   return (
-    <div className="border-2 border-white w-14 rounded-xl xl:hidden">
+    <div className='border-2 border-dark dark:border-white w-14 rounded-xl xl:hidden'>
       <div
         className={`${styles.hamburguer} relative ${
-          isOpen && styles["close-hamburguer"]
-        }`}
-      >
-        {/* Linahs abaixo: */}
+          isOpen && styles['close-hamburguer']
+        }`}>
         <div
-          className={`${styles.lines} ${isOpen && styles["line-top"]} ${
-            isOpen ? "h-1.25" : "h-0.5"
-          }`}
-        ></div>
+          className={`${styles.lines} ${isOpen && styles['line-top']} ${
+            isOpen ? 'h-1.25' : 'h-0.5'
+          }`}></div>
         <div
-          className={`${styles.lines} ${isOpen && styles["line-mid"]} ${
-            isOpen ? "h-1.25" : "h-0.5"
-          }`}
-        ></div>
+          className={`${styles.lines} ${isOpen && styles['line-mid']} ${
+            isOpen ? 'h-1.25' : 'h-0.5'
+          }`}></div>
         <div
-          className={`${styles.lines} ${isOpen && styles["line-bottom"]} ${
-            isOpen ? "h-1.25" : "h-0.5"
-          }`}
-        ></div>
+          className={`${styles.lines} ${isOpen && styles['line-bottom']} ${
+            isOpen ? 'h-1.25' : 'h-0.5'
+          }`}></div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Footer, Header, Highlight, BoxCard } from '../components';
 
 export default function Home() {
   return (
-    <main className='w-full max-w-full min-h-screen bg-dark'>
+    <main className='w-full max-w-full min-h-screen bg-white dark:bg-dark'>
       <Header />
       <Highlight />
       <BoxCard />
