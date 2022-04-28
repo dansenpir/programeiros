d0b760418600d7fd1e6f7d7efff42139f4626e39
dansenpir <dansenpir@outlook.com>
2022-04-28 18:43:31 -0300

    fix: text colors on light mode and on mouse hover

--- src/components/NavLink/index.tsx
+++ src/components/NavLink/index.tsx
@@ -1,19 +1,19 @@
 function NavLink() {
   return (
     <nav className='hidden xl:flex z-10 items-center mr-150'>
-      <ul className='ml-16 hidden ml-20 w-80 font-normal text-white sm:flex font-overpass'>
+      <ul className='ml-16 hidden ml-20 w-80 font-normal text-dark dark:text-white sm:flex font-overpass'>
         <li className='py-5'>
-          <a href='#' className='hover:text-btn-border'>
+          <a href='#' className='hover:text-blue'>
             Quem somos?
           </a>
         </li>
         <li className='py-5 mx-6'>
-          <a href='#' className='hover:text-btn-border'>
+          <a href='#' className='hover:text-blue'>
             Vagas
           </a>
         </li>
         <li className='py-5'>
-          <a href='#' className='hover:text-btn-border'>
+          <a href='#' className='hover:text-blue'>
             Guia de estudos
           </a>
         </li>
--- src/components/NavMobile/index.tsx
+++ src/components/NavMobile/index.tsx
@@ -1,19 +1,19 @@
 function NavMobile() {
   return (
     <nav className='absolute top-0 left-0 z-20 bg-bg-main w-full min-h-screen'>
-      <ul className='font-normal text-white font-overpass py-5 mt-20 animate__animated animate__fadeInLeft'>
+      <ul className='font-normal text-dark dark:text-white font-overpass py-5 mt-20 animate__animated animate__fadeInLeft'>
         <li className='p-5'>
-          <a href='#' className='hover:text-btn-border'>
+          <a href='#' className='hover:text-blue'>
             Quem somos?
           </a>
         </li>
         <li className='p-5'>
-          <a href='#' className='hover:text-btn-border'>
+          <a href='#' className='hover:text-blue'>
             Vagas
           </a>
         </li>
         <li className='p-5'>
-          <a href='#' className='hover:text-btn-border'>
+          <a href='#' className='hover:text-blue'>
             Guia de estudos
           </a>
         </li>
