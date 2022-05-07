f6420c32a04fa1b400f2407c10a6615743a5eba6
dansenpir <dansenpir@outlook.com>
2022-05-07 11:57:07 -0300

    refactor: move module css to tailwind classes

--- src/components/Hamburguer/index.tsx
+++ src/components/Hamburguer/index.tsx
@@ -8,21 +8,12 @@ const Hamburguer = ({ isOpen = false }: HamburProps) => {
   return (
     <div className='flex justify-center items-center border-2 border-dark dark:border-white w-14 rounded-xl xl:hidden'>
       <div
-        className={`${styles.hamburguer} relative ${
-          isOpen && styles['close-hamburguer']
+        className={`transition-all duration-300 ease-in w-6 h-4 my-4 z-50 cursor-pointer transform ${
+          isOpen && 'rotate-90'
         }`}>
-        <div
-          className={`bg-dark dark:bg-white ${styles.lines} ${
-            isOpen && styles['line-top']
-          }`}></div>
-        <div
-          className={`my-1.5 bg-dark dark:bg-white ${styles.lines} ${
-            isOpen && styles['line-mid']
-          }`}></div>
-        <div
-          className={`bg-dark dark:bg-white ${styles.lines} ${
-            isOpen && styles['line-bottom']
-          }`}></div>
+        <div className='bg-dark dark:bg-white w-full h-0.5'></div>
+        <div className='bg-dark dark:bg-white my-1.5 w-full h-0.5'></div>
+        <div className='bg-dark dark:bg-white w-full h-0.5'></div>
       </div>
     </div>
   );
