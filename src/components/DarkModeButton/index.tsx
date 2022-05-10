60b94f62cf1e085dd0ca999818eefd61295ac6bc
dansenpir <dansenpir@outlook.com>
2022-05-10 16:29:42 -0300

    fix: fix setTheme when click on DarkModeButton

--- src/components/DarkModeButton/index.tsx
+++ src/components/DarkModeButton/index.tsx
@@ -1,22 +1,17 @@
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
 
 const DarkModeButton = () => {
   const { theme, setTheme } = useTheme();
-  const [icon, setIcon] = useState(<FiSun />);
-
-  useEffect(() => {
-    theme === 'light' ? setIcon(<FiMoon />) : setIcon(<FiSun />);
-  }, [theme]);
 
   return (
     <button
       className='rounded-xl border-2 border-dark dark:border-white py-3 px-4 mx-5 lg:mx-0 ml-5 text-dark dark:text-white text-2xl hover:bg-dark dark:hover:bg-white hover:bg-opacity-5 dark:hover:bg-opacity-5 focus:bg-opacity-20 dark:focus:bg-opacity-20'
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-      {icon}
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      {theme === 'dark' ? <FiSun /> : <FiMoon />}
     </button>
   );
 };
 
 export { DarkModeButton };
+  
