e21005037a4442bf80771415f59e21c17ed27ced
dansenpir <dansenpir@outlook.com>
2022-05-14 17:37:46 -0300

    feat: links Vagas page on Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 const Navbar = () => (
   <nav className='hidden lg:flex items-center'>
     <ul className='flex font-overpass text-base xl:text-xl text-dark dark:text-white'>
       <li className='hover:text-blue cursor-pointer mr-6'>Quem somos</li>
-      <li className='hover:text-blue cursor-pointer mr-6'>Vagas</li>
+      <Link href='/vagas'>
+        <li className='hover:text-blue cursor-pointer mr-6'>Vagas</li>
+      </Link>
       <li className='hover:text-blue cursor-pointer'>Guia de estudos</li>
     </ul>
   </nav>
