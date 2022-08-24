b15c8f475858a9e7a77d1f2a57d7a530973d695a
Pedro Ribas <pedroibribas@gmail.com>
2022-08-24 17:55:02 -0400

    feat: updates GoToCard component

--- src/components/GoToCard/index.tsx
+++ src/components/GoToCard/index.tsx
@@ -1,15 +1,15 @@
 interface GoToCard {
   text: string;
-}
-
-const GoToCard = ({ text }: GoToCard) => {
-  return (
-    <button className={`hidden lg:block w-full py-5 pl-5 border border-green rounded-xl bg-green text-left`}>
-      <p className={`font-overpass text-white text-xl font-normal`}>
-        {text}
-      </p>
-    </button>
-  );
 };
+
+const GoToCard = ({ text }: GoToCard) => (
+  <button
+    className={`hidden lg:block w-full py-5 pl-5 border border-green rounded-xl bg-green bg-opacity-25 text-left`}
+  >
+    <p className={`font-overpass text-black dark:text-white text-xl font-normal`}>
+      {text}
+    </p>
+  </button>
+);
 
 export { GoToCard };
