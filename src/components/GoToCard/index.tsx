a3742ae9a5265979a727191b7e384550c269d757
Pedro Ribas <pedroibribas@gmail.com>
2022-08-25 11:48:49 -0400

    feat: updates GoToCard component

--- src/components/GoToCard/index.tsx
+++ src/components/GoToCard/index.tsx
@@ -1,12 +1,14 @@
 interface GoToCard {
   text: string;
-};
+}
 
 const GoToCard = ({ text }: GoToCard) => (
   <button
-    className={`hidden lg:block w-full py-5 pl-5 border border-green rounded-xl bg-green bg-opacity-25 text-left`}
+    className={`block w-full py-5 px-5 border border-green rounded-xl bg-green bg-opacity-25 text-left cursor-pointer`}
   >
-    <p className={`font-overpass text-black dark:text-white text-xl font-normal`}>
+    <p
+      className={`font-overpass text-black dark:text-white text-xl font-normal`}
+    >
       {text}
     </p>
   </button>
