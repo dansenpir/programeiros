25ff7a6e3268a8e604add1b32f5043ac762c0a5d
Pedro Ribas <pedroibribas@gmail.com>
2022-08-29 10:37:09 -0400

    feat: adds no-text button in SocialButton

--- src/components/SocialButton/index.tsx
+++ src/components/SocialButton/index.tsx
@@ -4,93 +4,60 @@ import Github from '../../../public/images/github_icon.svg';
 import Telegram from '../../../public/images/telegram_icon.svg';
 
 interface SocialButton {
-  text: string;
+  type: "telegram" | "discord" | "github";
+  hasText: boolean;
 }
 
-function SocialButton({ text }: SocialButton) {
+function SocialButton({ type, hasText }: SocialButton) {
+  const [color, setColor] = useState("");
+  const [hoverColor, setHoverColor] = useState("");
+  const [clickedColor, setClickedColor] = useState("");
+  const [url, setUrl] = useState("");
   const [isHover, setIsHover] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
-  const [link, setLink] = useState('');
-  const [color, setColor] = useState('');
-  const [hoverBackgroundColor, setHoverBackgroundColor] = useState('');
-  const [clickedBackgroundColor, setClickedBackgroundColor] = useState('');
-  const hoverHandle = () => setIsHover(true);
-  const clickHandle = () => setIsClicked(!isClicked);
-  const leaveHandle = () => {
-    setIsClicked(false);
-    setIsHover(false);
-  };
+  const svgStyles = `fill-current ${!isHover ? `text-${color}` : 'text-white'}`;
+
+  const hoverHandle = () => setIsHover(!isHover);
 
   useEffect(() => {
-    if (text.toLowerCase() === 'discord') {
-      setLink('https://discord.gg/pDxbmrzNaJ');
-      setColor('btn-discord');
-      setHoverBackgroundColor('btn-discord');
-      setClickedBackgroundColor('discord');
+    if (type === 'discord') {
+      setUrl('https://discord.gg/pDxbmrzNaJ');
+      setColor('discord');
+      setHoverColor('btn-discord');
+      setClickedColor('btn-discord');
     }
-    if (text.toLowerCase() === 'github') {
-      setLink('https://github.com/programeiros');
-      setColor('btn-github');
-      setHoverBackgroundColor('btn-github');
-      setClickedBackgroundColor('github');
+
+    if (type === 'github') {
+      setUrl('https://github.com/programeiros');
+      setColor('github');
+      setHoverColor('btn-github');
+      setClickedColor('btn-github');
     }
-    if (text.toLowerCase() === 'telegram') {
-      setLink('https://t.me/+p8PHV0M348k3NjE5');
-      setColor('btn-telegram');
-      setHoverBackgroundColor('btn-telegram');
-      setClickedBackgroundColor('telegram');
+
+    if (type === 'telegram') {
+      setUrl('https://t.me/+p8PHV0M348k3NjE5');
+      setColor('telegram');
+      setHoverColor('btn-telegram');
+      setClickedColor('btn-telegram');
     }
   }, []);
 
   return (
     <a
-      className='mb-14 sm:mb-0 mr-4 sm:mr-8'
-      href={link}
+      className={`flex items-center justify-center border border-${color} rounded-xl font-grotesk h-12 ${hasText ? 'w-32' : 'w-12'} hover:bg-${hoverColor} dark:hover:bg-${hoverColor} ${isHover && `hover:border-${hoverColor} dark:hover:border-${hoverColor} active:border-${clickedColor} active:bg-${clickedColor} dark:active:bg-${clickedColor}`} mb-14 sm:mb-0 mr-4 sm:mr-8 leading-5`}
+      href={url}
       target='_blank'
-      rel='noreferrer'>
-      <button
-        onMouseOver={() => hoverHandle()}
-        onMouseLeave={() => leaveHandle()}
-        onMouseDown={() => clickHandle()}
-        onMouseUp={() => clickHandle()}
-        className={`flex items-center justify-center border-1 border-solid border-${color} rounded-xl text-base font-grotesk font-normal leading-5 h-12 w-32 bg-white dark:bg-dark ${
-          isHover &&
-          `bg-${hoverBackgroundColor} dark:hover:bg-${hoverBackgroundColor}`
-        }
-          ${
-            isClicked &&
-            `bg-${clickedBackgroundColor} dark:focus:bg-${clickedBackgroundColor}`
-          }`}>
-        {text.toLowerCase() === 'discord' ? (
-          <Discord
-            {...((isHover || isClicked) && {
-              fill: 'white',
-            })}
-          />
-        ) : null}
-        {text.toLowerCase() === 'github' ? (
-          <Github
-            {...((isHover || isClicked) && {
-              fill: 'white',
-            })}
-          />
-        ) : null}
-        {text.toLowerCase() === 'telegram' ? (
-          <Telegram
-            {...((isHover || isClicked) && {
-              fill: 'white',
-            })}
-          />
-        ) : null}
-        <p
-          className={
-            isHover || isClicked
-              ? `ml-3 text-white dark:text-white`
-              : `ml-3 text-${color} dark:text-${color}`
-          }>
-          {text}
-        </p>
-      </button>
+      rel='noreferrer'
+      onMouseEnter={hoverHandle}
+      onMouseLeave={hoverHandle}
+    >
+      {type === 'telegram' && <Telegram className={svgStyles} />}
+      {type === 'discord' && <Discord className={svgStyles} />}
+      {type === 'github' && <Github className={svgStyles} />}
+      {hasText && (
+        <span className={`ml-3 capitalize ${isHover ? `text-white` : `text-${color}`}`}>
+          {type}
+        </span>
+      )}
     </a>
   );
 }
