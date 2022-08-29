8778b012e9a702e8888ada80238c22e020f23274
Pedro Ribas <pedroibribas@gmail.com>
2022-08-29 16:08:14 -0400

    fix: fixes styles and adds handler

--- src/components/NavbarMobile/index.tsx
+++ src/components/NavbarMobile/index.tsx
@@ -4,11 +4,12 @@ import Logo from '../../../public/images/logoFull.svg';
 const NavbarMobile = ({ isActive, clickHandle }) => (
   <>
     <div
-      className={`${isActive ? 'block' : 'hidden'} fixed top-0 left-0 w-screen h-screen bg-gray-white dark:bg-dark opacity-40 dark:opacity-40`}
+      className={`${isActive ? 'block' : 'hidden'} fixed top-0 left-0 w-screen h-screen bg-gray-white dark:bg-gray.900 opacity-60 dark:opacity-60`}
       onClick={clickHandle}
     />
-    <nav
-      className={`flex flex-col fixed top-0 left-0 z-50 bg-gray-white dark:bg-footer w-11/12 md:w-5/6 h-screen p-8 transition-transform duration-500 transform ${isActive ? 'translate-x-0' : '-translate-x-full'}`}
+    <aside
+      className={`flex flex-col fixed top-0 left-0 z-50 w-10/12 max-w-sm h-screen bg-gray-white dark:bg-gray.900 p-8 transition-transform duration-500 transform ${isActive ? 'translate-x-0' : '-translate-x-full'}`}
+      onClick={clickHandle}
     >
       <Logo className="fill-current dark:text-white" />
       <p className="mt-8 font-grotesk font-bold text-dark dark:text-white text-xl">
@@ -53,12 +54,12 @@ const NavbarMobile = ({ isActive, clickHandle }) => (
           />
         </li>
       </ul>
-      <div className="flex justify-between mt-auto mb-4">
+      <div className="flex justify-start gap-x-11 mt-auto mb-4">
         <SocialButton type="telegram" hasText={false} />
         <SocialButton type="discord" hasText={false} />
         <SocialButton type="github" hasText={false} />
       </div>
-    </nav>
+    </aside>
   </>
 );
 
