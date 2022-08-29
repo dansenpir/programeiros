b29984bd39c685a796eaa812f18fe24e49e2bf71
Pedro Ribas <pedroibribas@gmail.com>
2022-08-29 10:27:48 -0400

    refactor: cleans GoTo code

--- src/components/GoTo/index.tsx
+++ src/components/GoTo/index.tsx
@@ -4,41 +4,28 @@ import ArrowRight from '../../../public/images/arrow_right.svg';
 interface GoTo {
   text: string;
   color: string;
-  arrowColor: string;
   hoverColor: string;
   clickedColor: string;
+  margin: boolean;
 }
 
-const GoTo = ({ text, color, arrowColor, hoverColor, clickedColor }: GoTo) => {
+const GoTo = ({ text, color, hoverColor, clickedColor, margin }: GoTo) => {
   const [isHover, setIsHover] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
-  const hoverHandle = () => setIsHover(true);
-  const clickHandle = () => setIsClicked(!isClicked);
-  const leaveHandle = () => {
-    setIsClicked(false);
-    setIsHover(false);
-  };
+  const hoverHandle = () => setIsHover(!isHover);
   return (
     <button
-      onMouseOver={() => hoverHandle()}
-      onMouseLeave={() => leaveHandle()}
-      onMouseDown={() => clickHandle()}
-      onMouseUp={() => clickHandle()}
-      className={`mx-8 md:mx-16 h-12 w-48 border-1 border-${color} p-3 rounded-xl flex items-center justify-center ${
-        isHover && `bg-${hoverColor} dark:hover:bg-${hoverColor}`
-      }
-          ${isClicked && `bg-${clickedColor} dark:focus:bg-${clickedColor}`}`}>
+      className={`flex items-center h-12 px-3 ${margin && "mx-8 md:mx-16"} border border-${color} rounded-xl ${isHover && `hover:border-${hoverColor} hover:bg-${hoverColor} dark:hover:bg-${hoverColor} active:border-${clickedColor} active:bg-${clickedColor} dark:active:bg-${clickedColor}`}`}
+      onMouseEnter={hoverHandle}
+      onMouseLeave={hoverHandle}
+    >
       <p
-        className={`mr-3 font-grotesk font-normal text-base ${
-          isHover ? 'text-white dark:text-white' : `text-${color}`
-        }`}>
+        className={`font-grotesk ${isHover ? 'text-white' : `text-${color}`}`}
+      >
         {text}
       </p>
-      {isHover || isClicked ? (
-        <ArrowRight fill='white' />
-      ) : (
-        <ArrowRight fill={`${arrowColor}`} />
-      )}
+      <ArrowRight
+        className={`ml-3 fill-current ${isHover ? 'text-white' : `text-${color}`}`}
+      />
     </button>
   );
 };
