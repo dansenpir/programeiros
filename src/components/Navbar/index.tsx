c94611b33c28e4aa82e9379adc9ca56507c01185
alex-dll <alexsandrojr.303@gmail.com>
2022-08-29 02:32:37 -0300

    feat: :art: add hrefs to Navbar Mobile and Desktop

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,11 +3,21 @@ import Link from 'next/link';
 const Navbar = () => (
   <nav className="hidden lg:flex items-center">
     <ul className="flex font-overpass text-base xl:text-xl text-dark dark:text-white">
-      <li className="hover:text-blue cursor-pointer mr-6">Quem somos</li>
-      <Link href="/vagas" passHref>
-        <li className="hover:text-blue cursor-pointer mr-6">Vagas</li>
+      <Link href="/" passHref>
+        <a>
+          <li className="hover:text-blue cursor-pointer mr-6">Quem somos</li>
+        </a>
       </Link>
-      <li className="hover:text-blue cursor-pointer">Guia de estudos</li>
+      <Link href="/vagas" passHref>
+        <a>
+          <li className="hover:text-blue cursor-pointer mr-6">Vagas</li>
+        </a>
+      </Link>
+      <Link href="/" passHref>
+        <a>
+          <li className="hover:text-blue cursor-pointer">Guia de estudos</li>
+        </a>
+      </Link>
     </ul>
   </nav>
 );
